1cc2c39cecd0f469aaf5dff2b769ca9bf54182e7
Neville Dastur <neville@dastur.me.uk>
2015-05-31 21:42:28 +0100

    fix faq js error

--- src/static/js/faq.js
+++ src/static/js/faq.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   new WOW().init();
-}
+});
 
 $(".faq-q").click( function () {
   var container = $(this).parents(".faq-c");
   var answer = container.find(".faq-a");
   var trigger = container.find(".faq-t");
-  answer.slideToggle(200);
+  answer.slideToggle("slow");
   if (trigger.hasClass("faq-o")) {
     trigger.removeClass("faq-o");
   }
