e09d0a2cda33b164de6a4c6fb301c1727922af6c
Neville Dastur <neville@dastur.me.uk>
2015-07-02 18:06:27 +0100

    improve slider

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -2,37 +2,34 @@ new WOW().init();
 
 $(document).ready(function() {
     $("#starting-slider").owlCarousel({
-    autoPlay: 3000,
-    navigation : false, // Show next and prev buttons
-    slideSpeed : 900,
-    paginationSpeed : 2000,
-    rewindNav: false,
-    singleItem:true
+        autoPlay: 9000,
+        navigation : false, // Show next and prev buttons
+        slideSpeed : 2000,
+        paginationSpeed : 3000,
+        rewindNav: true,
+        singleItem: true
     });
 });
 
-$( function() {
+$(function() {
     // init Isotope
-    var $container = $('.isotope').isotope
-    ({
-    itemSelector: '.element-item',
-    layoutMode: 'fitRows'
+    var $container = $('.isotope').isotope({
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows'
     });
     // bind filter button click
-    $('#filters').on( 'click', 'button', function()
-    {
-    var filterValue = $( this ).attr('data-filter');
-    // use filterFn if matches value
-    $container.isotope({ filter: filterValue });
+    $('#filters').on( 'click', 'button', function() {
+        var filterValue = $( this ).attr('data-filter');
+        // use filterFn if matches value
+        $container.isotope({ filter: filterValue });
     });
+    
     // change is-checked class on buttons
-    $('.button-group').each( function( i, buttonGroup )
-    {
-    var $buttonGroup = $( buttonGroup );
-    $buttonGroup.on( 'click', 'button', function()
-    {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-    });
+    $('.button-group').each(function(i, buttonGroup) {
+        var $buttonGroup = $(buttonGroup);
+        $buttonGroup.on('click', 'button', function () {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $(this).addClass('is-checked');
+        });
     });
 });
