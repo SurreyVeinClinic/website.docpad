17dd38b45f13aadfc549f16deed5dacca463576a
Neville Dastur <nevilledastur@Nevilles-MacBook-Pro.local>
2015-08-11 17:28:51 +0100

    add ref

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
         e.preventDefault();
         console.log("Sending CTA");
 
+        // Ref: http://blog.danielw.co/Sending-Email-from-Github-Pages/
         $.ajax({
             url: atob("Ly9mb3Jtc3ByZWUuaW8vbmV2aWxsZUBkYXN0dXIubWUudWs=") //-"//formspree.io/neville@dastur.me.uk",
             type: "POST",
